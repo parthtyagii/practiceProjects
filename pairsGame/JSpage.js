e4d601577b353f140df351b54bef5ccf905413a9
Parth Tyagi <parthtyagicode@gmail.com>
2023-10-06 21:38:53 +0530

    JS functionality added

--- pairsGame/JSpage.js
+++ pairsGame/JSpage.js
@@ -1,3 +1,6 @@
+const indexes = [0, 1, 2, 3, 4];
+const indexCount = [6, 6, 6, 6, 6, 6];
+
 function shuffle(arr) {
     // Fisher-Yates shuffle algorithm
     for (let i = arr.length - 1; i > 0; i--) {
@@ -6,46 +9,55 @@ function shuffle(arr) {
     }
 }
 
-const indexes = [0, 1, 2, 3, 4, 5];
-const indexCount = [6, 6, 6, 6, 6, 6];
-
 //now
 const clickedCards = [];
 const check = (event) => {
     if (clickedCards.length === 0 || clickedCards[0] !== event.target) {
+        event.target.innerText = event.target.getAttribute("data-key");
         clickedCards.push(event.target);
     }
 
     if (clickedCards.length === 2) {
-        //logic...
-        if (clickedCards[0].getAttribute("data-key") !== clickedCards[1].getAttribute("data-key")) {
-            
-        }
+        setTimeout(() => {
+            //logic...
+            if (clickedCards[0].getAttribute("data-key") !== clickedCards[1].getAttribute("data-key")) {
+                clickedCards[0].innerText = "";
+                clickedCards[1].innerText = "";
+            }
 
-        //clear clickedCards array
-        clickedCards.pop();
-        clickedCards.pop();
+            //clear clickedCards array
+            clickedCards.pop();
+            clickedCards.pop();
+        }, 500);
     }
 
 }
 
+const mainContainer = document.querySelector("main .container");
 const cards = Array.from(document.querySelectorAll(".container span"));
-cards.map((card) => {
+for (let i = 0; i < 30; i++) {
     // assigning keys to spans
+    const el = document.createElement("span");
     shuffle(indexes);
+
     for (let x of indexes) {
         if (indexCount[x] > 0) {
-            card.setAttribute("data-key", "x");
+            const photu = document.createElement("img");
+            photu.setAttribute("src", `./icons/${x}.svg`);
+            el.setAttribute("data-key", `${x}`);
+            el.append(photu);
+            mainContainer.append(el);
             indexCount[x]--;
             break;
         }
     }
 
     // add event listeners to cards
-    card.addEventListener("click", check);
-});
+    el.addEventListener("click", check);
+};
 
 
+// stop-watch logic...
 
 
 
