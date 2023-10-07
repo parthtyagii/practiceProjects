34b47ac936b2665d26c86370ceb69b123e0c1da8
Parth Tyagi <parthtyagicode@gmail.com>
2023-10-07 11:01:58 +0530

    completed the Pairs Game Project

--- pairsGame/JSpage.js
+++ pairsGame/JSpage.js
@@ -1,6 +1,3 @@
-const indexes = [0, 1, 2, 3, 4];
-const indexCount = [6, 6, 6, 6, 6, 6];
-
 function shuffle(arr) {
     // Fisher-Yates shuffle algorithm
     for (let i = arr.length - 1; i > 0; i--) {
@@ -12,8 +9,10 @@ function shuffle(arr) {
 //now
 const clickedCards = [];
 const check = (event) => {
-    if (clickedCards.length === 0 || clickedCards[0] !== event.target) {
-        event.target.innerText = event.target.getAttribute("data-key");
+    const card = event.target.querySelector("img");
+
+    if (card.style.visibility === "hidden" && (clickedCards.length === 0 || clickedCards[0] !== event.target)) {
+        card.style.visibility = "visible";
         clickedCards.push(event.target);
     }
 
@@ -21,49 +20,71 @@ const check = (event) => {
         setTimeout(() => {
             //logic...
             if (clickedCards[0].getAttribute("data-key") !== clickedCards[1].getAttribute("data-key")) {
-                clickedCards[0].innerText = "";
-                clickedCards[1].innerText = "";
+                clickedCards[0].querySelector("img").style.visibility = "hidden";
+                clickedCards[1].querySelector("img").style.visibility = "hidden";
             }
 
             //clear clickedCards array
             clickedCards.pop();
             clickedCards.pop();
+            gameEnded();
         }, 500);
     }
-
 }
 
-const mainContainer = document.querySelector("main .container");
-const cards = Array.from(document.querySelectorAll(".container span"));
-for (let i = 0; i < 30; i++) {
-    // assigning keys to spans
-    const el = document.createElement("span");
-    shuffle(indexes);
+const mainContainer = document.querySelector(".container");
+const distribute = () => {
+    const indexes = [0, 1, 2, 3, 4];
+    const indexCount = [6, 6, 6, 6, 6];
+    for (let i = 0; i < 30; i++) {
+        // assigning keys to spans
+        const el = document.createElement("span");
+        const photu = document.createElement("img");
+        shuffle(indexes);
 
-    for (let x of indexes) {
-        if (indexCount[x] > 0) {
-            const photu = document.createElement("img");
-            photu.setAttribute("src", `./icons/${x}.svg`);
-            el.setAttribute("data-key", `${x}`);
-            el.append(photu);
-            mainContainer.append(el);
-            indexCount[x]--;
-            break;
+        for (let x of indexes) {
+            if (indexCount[x] > 0) {
+                photu.setAttribute("src", `./icons/${x}.svg`);
+                photu.style.visibility = "hidden";
+                el.setAttribute("data-key", `${x}`);
+                indexCount[x]--;
+                break;
+            }
         }
+        el.append(photu);
+        mainContainer.append(el);
+
+        // add event listeners to cards
+        el.addEventListener("click", check);
+    };
+}
+
+const gameEnded = () => {
+    const cards = Array.from(document.querySelectorAll(".container span"));
+    let count = 0;
+    cards.map((c) => {
+        if (c.querySelector("img").style.visibility === "visible") {
+            count++;
+        }
+    });
+
+    if (count === 30) {
+        clearInterval(stopWatch);
+        document.querySelector(".modal").style.display = "flex";
+        document.querySelector(".modalContainer span").innerText = "Your Time :- " + time;
+        document.querySelector(".modalContainer button").innerText = "Try Again";
     }
-
-    // add event listeners to cards
-    el.addEventListener("click", check);
-};
-
+}
 
 // stop-watch logic...
+let stopWatch;
+let minutes = 0, seconds = 0;
+let time = "";
 const watch = () => {
     const timer = document.querySelector("nav");
-    let minutes = 0, seconds = 0;
     let watchMin = "", watchSec = "";
 
-    setInterval(() => {
+    stopWatch = setInterval(() => {
         seconds++;
         if (seconds < 10) {
             watchSec = "0" + seconds;
@@ -85,10 +106,30 @@ const watch = () => {
         }
 
         timer.innerText = watchMin + " : " + watchSec;
+        time = watchMin + " : " + watchSec;
     }, 1000);
 }
 
-// watch();
+//pop-up logic
+let flag = false;
+const popup = document.querySelector(".modal .modalContainer button");
+popup.addEventListener("click", () => {
+    if (flag) {
+        const container = document.querySelector(".container");
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
+    else {
+        flag = true;
+    }
+
+    distribute();
+    minutes = 0; seconds = 0;
+    document.querySelector("nav").innerText = "00 : 00";
+    document.querySelector(".modal").style.display = "none";
+    watch();
+});
 
 
 
